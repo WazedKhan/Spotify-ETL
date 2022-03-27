1b6143a77e81f88de5ddd338211230a6cda56ed1
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-03-27 22:43:23 +0600

    garbage in and out (transfom of data)

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-import json
-import sqlite3
+
 import datetime
-from time import time
 import requests
-import sqlalchemy
 import pandas as pd
-from sqlalchemy.orm import sessionmaker
 
 
 DATABASE_LOCATION = "sqlite:///tracks_list.sqlite"
 USER_ID = "314zhyszvimzwwdaq5ehbsalknri"
-TOKEN = "BQC2TUPnm6xRdkv60QJa7gCXHa0QTQd9hdkU6k4uIrzK8IdMFuMbpSdvA1F7vFdnWw9h1eV3lZhhrCb7hsmgSmVma5BTFlohxpv-LTdK_tcGH1Sn-iaYvAi2HQIIibqyBu6ZGHO8kKC4mTh5ft_Ax4irUfmhLUUJjTsEu5fq"
+TOKEN = "BQDdztbpsBImL1t-LkOelmi013GBVJqXQE2-GtJnBfZa_PyQpxYS5of-uWBxwwsUaY68YcMQ7BWvue-7Jz7edhOu1hJdt5tOSTbtc8Lds1KgyIcuBcDezQSIe-X7TzE8i7fybR2SU8hxl53t5-chITgDsk6G3jURWE2vuWOh"
 
 if __name__ == "__main__":
     headers = {
@@ -19,6 +15,24 @@ if __name__ == "__main__":
         "Authorization" : "Bearer {token}".format(token=TOKEN)
     }
 
+
+def data_validation(df: pd.DataFrame):
+    # if df is empty
+    if df.empty:
+        print("No Song Lisented yesterday")
+        return False
+    # check if data duplicate
+    if pd.Series(df['played_at']).is_unique:
+        pass
+    else:
+        raise Exception('Dulicated Song')
+    # chcek is 
+    if df.isnull().values.any():
+        raise Exception('Null Value Found')
+    
+    print(df)
+
+
 today = datetime.datetime.now()
 yesterday = today - datetime.timedelta(days=1)
 yesterday_unix_timestamp = int(yesterday.timestamp())*1000
@@ -26,17 +40,19 @@ yesterday_unix_timestamp = int(yesterday.timestamp())*1000
 r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers=headers)
 
 data = r.json()
-
 song_name = []
 artist_name = []
 played_at_list = []
 timestamps = []
-
-for song in data['items']:
-    song_name.append(song['track']['name'])
-    artist_name.append(song['track']['album']['artists'][0]['name'])
-    played_at_list.append(song['played_at'])
-    timestamps.append(song['played_at'][0:10])
+  
+try:
+    for song in data['items']:
+        song_name.append(song['track']['name'])
+        artist_name.append(song['track']['album']['artists'][0]['name'])
+        played_at_list.append(song['played_at'])
+        timestamps.append(song['played_at'][0:10])
+except KeyError:
+    print('Spotify Token Expired')
 
 
 song_dic = {
@@ -48,4 +64,4 @@ song_dic = {
 
 
 df = pd.DataFrame(song_dic, columns=['song_name','artist_name','played_at','timestamp'])
-df.to_csv('songs.csv')
+data_validation(df)
